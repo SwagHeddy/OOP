385032dc63d743a74ada4d76acce518b83b4bd36
SwagHeddy <54781706+SwagHeddy@users.noreply.github.com>
2023-10-10 22:24:15 +1000

    Update poker.cpp

--- poker/poker.cpp
+++ poker/poker.cpp
@@ -1,92 +1,223 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <random>
 #include <string>
+#include <map>
+#include <random>
 
-struct Card {
-    std::string suit;
-    std::string value;
+// Определяем класс карты
+class Card {
+public:
+    enum Rank { ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
+    enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES };
+
+    Card(Rank rank = ACE, Suit suit = SPADES) : m_Rank(rank), m_Suit(suit) {}
+
+    int getValue() const {
+        return m_Rank;
+    }
+
+    void display() const {
+        std::string rankStr = "";
+        std::string suitStr = "";
+
+        switch (m_Rank) {
+        case ACE:
+            rankStr = "A";
+            break;
+        case JACK:
+            rankStr = "J";
+            break;
+        case QUEEN:
+            rankStr = "Q";
+            break;
+        case KING:
+            rankStr = "K";
+            break;
+        default:
+            rankStr = std::to_string(m_Rank);
+            break;
+        }
+
+        switch (m_Suit) {
+        case CLUBS:
+            suitStr = "C";
+            break;
+        case DIAMONDS:
+            suitStr = "D";
+            break;
+        case HEARTS:
+            suitStr = "H";
+            break;
+        case SPADES:
+            suitStr = "S";
+            break;
+        }
+
+        std::cout << rankStr << suitStr << " ";
+    }
+
+private:
+    Rank m_Rank;
+    Suit m_Suit;
 };
 
-std::vector<Card> generateDeck() {
-    std::vector<Card> deck;
-    std::string suits[] = { "C", "D", "H", "S" };
-    std::string values[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-
-    for (const auto& suit : suits) {
-        for (const auto& value : values) {
-            deck.push_back({ suit, value });
+// Определяем класс колоды карт
+class Deck {
+public:
+    Deck() {
+        for (int suit = Card::CLUBS; suit <= Card::SPADES; ++suit) {
+            for (int rank = Card::ACE; rank <= Card::KING; ++rank) {
+                m_Cards.push_back(Card(static_cast<Card::Rank>(rank), static_cast<Card::Suit>(suit)));
+            }
         }
     }
 
-    return deck;
-}
-
-std::vector<Card> generatePlayerCards(std::vector<Card>& deck, int numCards) {
-    std::vector<Card> playerCards;
-
-    std::random_device rd;
-    std::mt19937 generator(rd());
-
-    for (int i = 0; i < numCards; i++) {
-        std::uniform_int_distribution<int> distribution(0, deck.size() - 1);
-        int randomIndex = distribution(generator);
-
-        playerCards.push_back(deck[randomIndex]);
-        deck.erase(deck.begin() + randomIndex);
+    void generateDeck() {
+        std::random_device rd;
+        std::mt19937 eng(rd());
+        std::shuffle(m_Cards.begin(), m_Cards.end(), eng);
     }
 
-    return playerCards;
-}
+    const Card& draw() {
+        // Удаление карты из колоды
+        const Card& card = m_Cards.back();
+        m_Cards.pop_back();
 
-int calculatePoints(const std::vector<Card>& playerCards) {
+        return card;
+    }
+
+    class iterator : public std::iterator<std::input_iterator_tag, Card> {
+    public:
+        iterator(std::vector<Card>::iterator it) : m_Iterator(it) {}
+
+        iterator operator+(int n) const {
+            iterator temp = *this;
+            for (int i = 0; i < n; i++) {
+                ++temp; // Перемещаем итератор вперед
+            }
+            return temp;
+        }
+
+        iterator& operator++() {
+            ++m_Iterator;
+            return *this;
+        }
+
+        bool operator!=(const iterator& other) const {
+            return m_Iterator != other.m_Iterator;
+        }
+
+        const Card& operator*() const {
+            return *m_Iterator;
+        }
+
+    private:
+        std::vector<Card>::iterator m_Iterator;
+    };
+
+    iterator begin() {
+        return iterator(m_Cards.begin());
+    }
+
+    iterator end() {
+        return iterator(m_Cards.end());
+    }
+
+private:
+    std::vector<Card> m_Cards;
+};
+
+// Определяем класс адаптера для итератора колоды карт
+class DeckIteratorAdapter {
+public:
+    DeckIteratorAdapter(Deck::iterator iterator) : m_Iterator(iterator) {}
+
+    DeckIteratorAdapter& operator++() {
+        ++m_Iterator;
+        return *this;
+    }
+
+    bool operator!=(const DeckIteratorAdapter& other) const {
+        return m_Iterator != other.m_Iterator;
+    }
+
+    const Card& operator*() const {
+        return *m_Iterator;
+    }
+
+private:
+    Deck::iterator m_Iterator;
+};
+
+
+// Функция подсчета очков игрока
+int calculatePoints(const std::vector<Card>& hand) {
     int points = 0;
-    for (const auto& card : playerCards) {
-        if (card.value == "A") {
-            points += 1;
-        }
-        else if (card.value == "J" || card.value == "Q" || card.value == "K") {
-            points += 10;
-        }
-        else {
-            points += stoi(card.value);
+    std::map<int, int> rankCounts;
+
+    for (const Card& card : hand) {
+        rankCounts[card.getValue()]++;
+    }
+
+    for (const auto& count : rankCounts) {
+        if (count.second >= 2) { // Изменить условие на count.second >= 2
+            points += count.second; // Добавить количество карт к points
         }
     }
+
     return points;
 }
 
-void printPlayerCards(const std::vector<Card>& playerCards, int playerNumber) {
-    std::cout << "Player " << playerNumber << " cards: ";
-    for (const auto& card : playerCards) {
-        std::cout << card.value << card.suit << " ";
-    }
-    std::cout << std::endl;
-}
-
-int main() {
-    std::vector<Card> deck = generateDeck();
-    std::vector<Card> player1Cards = generatePlayerCards(deck, 5);
-    std::vector<Card> player2Cards = generatePlayerCards(deck, 5);
-
-    printPlayerCards(player1Cards, 1);
-    printPlayerCards(player2Cards, 2);
-
-    int player1Points = calculatePoints(player1Cards);
-    int player2Points = calculatePoints(player2Cards);
-
-    std::cout << "Player 1 points: " << player1Points << std::endl;
-    std::cout << "Player 2 points: " << player2Points << std::endl;
-
+// Функция объявления победителя
+void determineWinner(int player1Points, int player2Points) {
     if (player1Points > player2Points) {
         std::cout << "Player 1 wins!" << std::endl;
     }
-    else if (player1Points < player2Points) {
+    else if (player2Points > player1Points) {
         std::cout << "Player 2 wins!" << std::endl;
     }
     else {
         std::cout << "It's a tie!" << std::endl;
     }
+}
 
-    return 0;
+// Основная функция игры
+void playPoker() {
+    Deck deck;
+    deck.generateDeck();
+
+    int handSize = 5;
+
+    std::vector<Card> player1Hand;
+    std::vector<Card> player2Hand;
+
+    std::cout << "Player 1 cards: ";
+    for (DeckIteratorAdapter it = deck.begin(); it != deck.begin() + handSize; ++it) {
+        const Card& card = *it;
+        card.display();
+        player1Hand.push_back(card);
+    }
+    std::cout << std::endl;
+
+    std::cout << "Player 2 cards: ";
+    for (DeckIteratorAdapter it = deck.begin(); it != deck.begin() + (2 * handSize); ++it) {
+        const Card& card = *it;
+        card.display();
+        player2Hand.push_back(card);
+    }
+    std::cout << std::endl;
+
+    int player1Points = calculatePoints(player1Hand);
+    int player2Points = calculatePoints(player2Hand);
+
+    std::cout << "Player 1 points: " << player1Points << std::endl;
+    std::cout << "Player 2 points: " << player2Points << std::endl;
+
+    determineWinner(player1Points, player2Points);
+}
+
+int main() {
+    playPoker();
+    system("pause");
 }
